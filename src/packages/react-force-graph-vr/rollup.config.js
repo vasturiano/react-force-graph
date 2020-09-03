1647d3fa112558aaf74f801ca8fc908c6cf75852
vasco <vastur@gmail.com>
2020-09-02 22:46:01 -0700

    Adjust rollup cfg

--- src/packages/react-force-graph-vr/rollup.config.js
+++ src/packages/react-force-graph-vr/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
-import replace from 'rollup-plugin-replace';
-import babel from 'rollup-plugin-babel';
+import replace from '@rollup/plugin-replace';
+import babel from '@rollup/plugin-babel';
 import { terser } from "rollup-plugin-terser";
 import dts from 'rollup-plugin-dts';
 import { name, homepage, version, dependencies, peerDependencies } from './package.json';
