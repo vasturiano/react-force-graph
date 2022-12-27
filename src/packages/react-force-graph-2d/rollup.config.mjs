0d85907c0c8f5b863a8242917f83531098d12493
vasco <vastur@gmail.com>
2022-12-27 01:41:11 +0000

    Update terser plugin

--- src/packages/react-force-graph-2d/rollup.config.mjs
+++ src/packages/react-force-graph-2d/rollup.config.mjs
@@ -2,7 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonJs from '@rollup/plugin-commonjs';
 import replace from '@rollup/plugin-replace';
 import babel from '@rollup/plugin-babel';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import dts from 'rollup-plugin-dts';
 
 import pkg from './package.json' assert { type: 'json' };
